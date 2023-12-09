d8baa458a55b510431a861e7d03d93153a5328e4
BCanOzen <140835648+BCanOzen@users.noreply.github.com>
2023-12-09 23:10:18 +0300

    Add files via upload

---
+++ Turnike_Sistemi/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Turnike_Sistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.lblWorkerName = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(12, -20);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(521, 629);
+            this.pictureBox3.TabIndex = 18;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, -17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 626);
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Turnike_Sistemi.Properties.Resources.monitor_hd_png_monitor_png_hd_png_image_2400;
+            this.pictureBox2.Location = new System.Drawing.Point(557, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(595, 605);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 612);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(543, 52);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "\"Data Source=DESKTOP-VTJLH0D;Initial Catalog=HR;\r\nIntegrated Security=True\"";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(557, 615);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(595, 29);
+            this.textBox1.TabIndex = 23;
+            this.textBox1.Text = "e0769b5b-0aae-4479-90a6-edd0f4374847";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Lime;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(427, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 55);
+            this.button1.TabIndex = 24;
+            this.button1.Text = "GİRİŞ";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(98, 26);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(83, 52);
+            this.button2.TabIndex = 25;
+            this.button2.Text = "ÇIKIŞ";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(50, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(24, 18);
+            this.panel1.TabIndex = 26;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(271, 90);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(24, 18);
+            this.panel2.TabIndex = 27;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(234, 38);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(24, 18);
+            this.panel3.TabIndex = 27;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(133, 264);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(24, 18);
+            this.panel4.TabIndex = 27;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.lblStatus);
+            this.panel5.Controls.Add(this.lblDate);
+            this.panel5.Controls.Add(this.lblWorkerName);
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Controls.Add(this.label3);
+            this.panel5.Location = new System.Drawing.Point(588, 38);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(529, 341);
+            this.panel5.TabIndex = 28;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDate.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblDate.Location = new System.Drawing.Point(183, 63);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(41, 31);
+            this.lblDate.TabIndex = 7;
+            this.lblDate.Text = "...";
+            // 
+            // lblWorkerName
+            // 
+            this.lblWorkerName.AutoSize = true;
+            this.lblWorkerName.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblWorkerName.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblWorkerName.Location = new System.Drawing.Point(183, 29);
+            this.lblWorkerName.Name = "lblWorkerName";
+            this.lblWorkerName.Size = new System.Drawing.Size(41, 31);
+            this.lblWorkerName.TabIndex = 6;
+            this.lblWorkerName.Text = "...";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(68, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tarih:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(29, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ad Soyad:";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblStatus.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lblStatus.Location = new System.Drawing.Point(68, 162);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(21, 20);
+            this.lblStatus.TabIndex = 8;
+            this.lblStatus.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 665);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel4);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.Label lblWorkerName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblStatus;
+    }
+}
+
---
+++ Turnike_Sistemi/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Turnike_Sistemi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public clsData data = new clsData();
+
+        private void hideIcons()
+        {
+            panel1.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = true;
+            panel4.Visible = true;
+            pictureBox3.Visible = true;
+            Update();
+        }
+        private void showIcons(string io, bool status)
+
+        {
+            panel1.Visible = true;
+            panel2.Visible = true;
+            panel3.Visible = true;
+            panel4.Visible = true;
+            pictureBox3.Visible = true;
+            Update();
+            if (io == "C")
+            {
+                if (status == true)
+                {
+                    panel2.Visible = false;
+                    pictureBox3.Visible = false;
+
+                }
+                if (status == false)
+                {
+                    panel1.Visible = false;
+                }
+            }
+            if (io == "G")
+            {
+                if (status == true)
+                {
+                    panel4.Visible = false;
+                    pictureBox3.Visible = false;
+
+                }
+                if (status == false)
+                {
+                    panel3.Visible = false;
+                }
+            }
+            if (io == "")
+            {
+                panel1.Visible = true;
+                panel2.Visible = true;
+                panel3.Visible = true;
+                panel4.Visible = true;
+                Update();
+            }
+
+        }
+        string workerCode = "";
+        int workerId;
+        string workerName = "";
+        DateTime dt;
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            DateTime dt = DateTime.Now.Date.AddHours(8);
+            DateTime lastdt = dt;
+            workerCode = textBox1.Text;
+            string sql;
+            //Son hareket sorgulama
+            sql = "SELECT TOP 1 * FROM WORKERTRANSACTIONS WHERE WORKERID IN (SELECT ID FROM WORKERS WHERE WORKERBARCODE='" + workerCode + "') AND DATE_>='" + DateTime.Now.ToString("yyyy-MM-dd") + "' ORDER BY DATE_ DESC, ID DESC";
+            ds = data.fill(CommandType.Text, sql);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                lastdt = Convert.ToDateTime(ds.Tables[0].Rows[0]["DATE_"]);
+            }
+            if (lastdt != null)
+            {
+                dt = lastdt.AddHours(0.3);
+            }
+
+
+            SqlParameter spWorkerBarcode = new SqlParameter("@WORKERBARCODE", SqlDbType.VarChar);
+            spWorkerBarcode.Value = workerCode;
+
+            SqlParameter spIOType = new SqlParameter("@IOTYPE", SqlDbType.VarChar);
+            spIOType.Value = "G";
+
+            SqlParameter spGateId = new SqlParameter("@GATEID", SqlDbType.Int);
+            spGateId.Value = 1;
+            SqlParameter spDate = new SqlParameter("@DATE", SqlDbType.DateTime);
+            spDate.Value = dt;
+
+            try
+            {
+                ds.Clear();
+                ds.Reset();
+
+                ds = data.fill(CommandType.StoredProcedure, "SP_WORKER_INOUT", spWorkerBarcode, spIOType, spDate, spGateId);
+                lblWorkerName.Text = ds.Tables[0].Rows[0]["WORKERNAME"].ToString();
+                lblDate.Text = Convert.ToDateTime(ds.Tables[0].Rows[0]["DATE_"]).ToString("yyyy-MM-dd HH:mm:ss");
+                lblStatus.Text = "Giriş başarılı.";
+                lblStatus.ForeColor = Color.Green;
+                Update();
+                showIcons("G", true);
+                Update();
+                System.Threading.Thread.Sleep(2000);
+                lblStatus.Text = "";
+                hideIcons();
+                Update();
+            }
+            catch (Exception ex)
+            {
+
+                lblStatus.Text = ex.Message;
+                lblStatus.ForeColor = Color.Red;
+                Update();
+                showIcons("G", false);
+                Update();
+                System.Threading.Thread.Sleep(2000);
+                lblStatus.Text = "";
+                lblDate.Text = "...";
+                lblWorkerName.Text = "...";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+
+            DateTime dt = DateTime.Now.Date.AddHours(8);
+            DateTime lastdt = dt;
+            workerCode = textBox1.Text;
+            string sql;
+            //Son hareket sorgulama
+            sql = "SELECT TOP 1 * FROM WORKERTRANSACTIONS WHERE WORKERID IN (SELECT ID FROM WORKERS WHERE WORKERBARCODE='" + workerCode + "') AND DATE_>='" + DateTime.Now.ToString("yyyy-MM-dd") + "' ORDER BY DATE_ DESC, ID DESC";
+            ds = data.fill(CommandType.Text, sql);
+
+            if (ds.Tables[0].Rows.Count > 0)
+            {
+                lastdt = Convert.ToDateTime(ds.Tables[0].Rows[0]["DATE_"]);
+            }
+            if (lastdt != null)
+            {
+                dt = lastdt.AddHours(0.3);
+            }
+
+
+            SqlParameter spWorkerBarcode = new SqlParameter("@WORKERBARCODE", SqlDbType.VarChar);
+            spWorkerBarcode.Value = workerCode;
+
+            SqlParameter spIOType = new SqlParameter("@IOTYPE", SqlDbType.VarChar);
+            spIOType.Value = "C";
+
+            SqlParameter spDate = new SqlParameter("@DATE", SqlDbType.DateTime);
+            spDate.Value = dt;
+            SqlParameter spGateId = new SqlParameter("@GATEID", SqlDbType.Int);
+            spGateId.Value = 1;
+            try
+            {
+                ds = data.fill(CommandType.StoredProcedure, "SP_WORKER_INOUT", spWorkerBarcode, spIOType, spDate, spGateId);
+
+
+                lblWorkerName.Text = ds.Tables[0].Rows[0]["WORKERNAME"].ToString();
+                lblDate.Text = Convert.ToDateTime(ds.Tables[0].Rows[0]["DATE_"]).ToString("yyyy-MM-dd HH:mm:ss");
+                lblStatus.Text = "Çıkış başarılı.";
+                lblStatus.ForeColor = Color.Green;
+                showIcons("C", true);
+                Update();
+                System.Threading.Thread.Sleep(2000);
+                lblStatus.Text = "";
+                hideIcons();
+
+            }
+            catch (Exception ex)
+            {
+
+                lblStatus.Text = ex.Message;
+                lblStatus.ForeColor = Color.Red;
+                showIcons("C", false);
+                Update();
+                System.Threading.Thread.Sleep(2000);
+                lblStatus.Text = "";
+                hideIcons();
+                Update();
+            }
+        }
+    }
+}
